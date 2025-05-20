c9aaa966bede7ebd40c6eba616c0f622e5a1a548
Georgina Kimani <njokikimani001@gmail.com>
2025-05-20 17:29:57 +0300

    updated email and messages sent

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c845729d29525bc5"; }
+/******/ 		__webpack_require__.h = function() { return "9582a1431853aa41"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
